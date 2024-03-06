35bddc9819d1dc79083f03a10ae0a72f00b00dd7
Josh Griggs <jbg4m@mtmail.mtsu.edu>
2024-03-05 22:22:16 -0600

    login page on form 2

--- FinalProject24/loginForm2.Designer.cs
+++ FinalProject24/loginForm2.Designer.cs
@@ -28,17 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            linkLabel1 = new LinkLabel();
+            label4 = new Label();
             button1 = new Button();
+            panel1.SuspendLayout();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Gainsboro;
+            panel1.Controls.Add(linkLabel1);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(256, 31);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(416, 431);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(94, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(196, 37);
+            label1.TabIndex = 1;
+            label1.Text = "Welcome Back!";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ScrollBar;
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(131, 114);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(262, 29);
+            textBox1.TabIndex = 4;
+            textBox1.Text = "Enter your email";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(53, 116);
+            label2.Name = "label2";
+            label2.Size = new Size(51, 21);
+            label2.TabIndex = 3;
+            label2.Text = "Email:";
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.ScrollBar;
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(131, 176);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(262, 29);
+            textBox2.TabIndex = 6;
+            textBox2.Text = "Enter your password";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(25, 178);
+            label3.Name = "label3";
+            label3.Size = new Size(79, 21);
+            label3.TabIndex = 5;
+            label3.Text = "Password:";
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Font = new Font("Segoe UI", 12F);
+            linkLabel1.Location = new Point(260, 345);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(65, 21);
+            linkLabel1.TabIndex = 10;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Sign Up";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F);
+            label4.Location = new Point(82, 345);
+            label4.Name = "label4";
+            label4.Size = new Size(171, 21);
+            label4.TabIndex = 9;
+            label4.Text = "Don't have an account?";
             // 
             // button1
             // 
-            button1.Location = new Point(444, 386);
+            button1.BackColor = Color.Black;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(82, 296);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            button1.Size = new Size(262, 33);
+            button1.TabIndex = 8;
+            button1.Text = "Sign In";
+            button1.UseVisualStyleBackColor = false;
             // 
             // loginForm
             // 
@@ -46,14 +148,24 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(124, 172, 175);
             ClientSize = new Size(984, 561);
-            Controls.Add(button1);
+            Controls.Add(panel1);
             Name = "loginForm";
             Text = "loginForm2";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
+        private Panel panel1;
+        private LinkLabel linkLabel1;
+        private Label label4;
         private Button button1;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label1;
     }
 }
