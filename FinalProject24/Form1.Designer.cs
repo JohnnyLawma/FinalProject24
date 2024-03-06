6e6a8d20fe54f1bbedee84202728072f2f585f2c
dwasd7 <diwasdulal2002@gmail.com>
2024-03-05 23:04:12 -0600

    MainMenuPage

--- FinalProject24/Form1.Designer.cs
+++ FinalProject24/Form1.Designer.cs
@@ -28,18 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            profilePictureBox = new PictureBox();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            nameLabel = new Label();
+            button4 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)profilePictureBox).BeginInit();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.DarkCyan;
+            panel1.Controls.Add(profilePictureBox);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(nameLabel);
+            panel1.Controls.Add(button4);
+            panel1.Location = new Point(1, 0);
+            panel1.Margin = new Padding(2, 1, 2, 1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(100, 600);
+            panel1.TabIndex = 1;
+            // 
+            // profilePictureBox
+            // 
+            profilePictureBox.BackColor = Color.MintCream;
+            profilePictureBox.Location = new Point(12, 30);
+            profilePictureBox.Margin = new Padding(2, 1, 2, 1);
+            profilePictureBox.Name = "profilePictureBox";
+            profilePictureBox.Size = new Size(75, 53);
+            profilePictureBox.TabIndex = 5;
+            profilePictureBox.TabStop = false;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(2, 205);
+            button3.Margin = new Padding(2, 1, 2, 1);
+            button3.Name = "button3";
+            button3.Size = new Size(96, 42);
+            button3.TabIndex = 2;
+            button3.Text = "Setting";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(2, 161);
+            button2.Margin = new Padding(2, 1, 2, 1);
+            button2.Name = "button2";
+            button2.Size = new Size(96, 42);
+            button2.TabIndex = 1;
+            button2.Text = "Payment";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(2, 115);
+            button1.Margin = new Padding(2, 1, 2, 1);
+            button1.Name = "button1";
+            button1.Size = new Size(96, 42);
+            button1.TabIndex = 0;
+            button1.Text = "Order";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // nameLabel
+            // 
+            nameLabel.Font = new Font("Segoe UI", 10.125F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            nameLabel.Location = new Point(12, 84);
+            nameLabel.Margin = new Padding(2, 0, 2, 0);
+            nameLabel.Name = "nameLabel";
+            nameLabel.Size = new Size(75, 30);
+            nameLabel.TabIndex = 4;
+            nameLabel.Text = "Name";
+            nameLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(2, 250);
+            button4.Margin = new Padding(2, 1, 2, 1);
+            button4.Name = "button4";
+            button4.Size = new Size(96, 42);
+            button4.TabIndex = 3;
+            button4.Text = "Sign Out";
+            button4.UseVisualStyleBackColor = true;
             // 
             // mainPageForm1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(984, 561);
+            Controls.Add(panel1);
             Name = "mainPageForm1";
             Text = "mainPageForm1";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)profilePictureBox).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private PictureBox profilePictureBox;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Label nameLabel;
+        private Button button4;
     }
 }
