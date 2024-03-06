75fa1f0ebff823f9f464ca4309748285c95656b5
dwasd7 <diwasdulal2002@gmail.com>
2024-03-05 23:28:39 -0600

    Payment Details

--- FinalProject24/PaymentDetailsUserControl1.Designer.cs
+++ FinalProject24/PaymentDetailsUserControl1.Designer.cs
@@ -28,17 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ScrollBar;
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(346, 216);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(262, 29);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.ScrollBar;
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(346, 282);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(201, 29);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.ScrollBar;
+            textBox3.BorderStyle = BorderStyle.FixedSingle;
+            textBox3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(346, 343);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(345, 29);
+            textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = SystemColors.ScrollBar;
+            textBox4.BorderStyle = BorderStyle.FixedSingle;
+            textBox4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox4.Location = new Point(553, 282);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(55, 29);
+            textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            textBox5.BackColor = SystemColors.ScrollBar;
+            textBox5.BorderStyle = BorderStyle.FixedSingle;
+            textBox5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox5.Location = new Point(614, 282);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(77, 29);
+            textBox5.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            textBox6.BackColor = SystemColors.ScrollBar;
+            textBox6.BorderStyle = BorderStyle.FixedSingle;
+            textBox6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox6.Location = new Point(614, 216);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(77, 29);
+            textBox6.TabIndex = 10;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(122, 92);
+            label1.Name = "label1";
+            label1.Size = new Size(226, 37);
+            label1.TabIndex = 11;
+            label1.Text = "Payment Details";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semilight", 15.25F);
+            label2.Location = new Point(122, 212);
+            label2.Name = "label2";
+            label2.Size = new Size(138, 30);
+            label2.TabIndex = 12;
+            label2.Text = "Main Address";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semilight", 15.25F);
+            label3.Location = new Point(122, 282);
+            label3.Name = "label3";
+            label3.Size = new Size(137, 30);
+            label3.TabIndex = 13;
+            label3.Text = "Card Number";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semilight", 15.25F);
+            label4.Location = new Point(122, 339);
+            label4.Name = "label4";
+            label4.Size = new Size(185, 30);
+            label4.TabIndex = 14;
+            label4.Text = "Card Holder Name";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Black;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(379, 425);
+            button1.Name = "button1";
+            button1.Size = new Size(187, 41);
+            button1.TabIndex = 15;
+            button1.Text = "Save";
+            button1.UseVisualStyleBackColor = false;
             // 
             // PaymentUserControl1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Name = "PaymentUserControl1";
             Size = new Size(900, 600);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button button1;
     }
 }
