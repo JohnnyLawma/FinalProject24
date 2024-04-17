84db428d485ab1366389316056b9b5c52528ee1b
Josh Griggs <jbg4m@mtmail.mtsu.edu>
2024-04-17 14:33:47 -0500

    spelling

--- FinalProject24/ProductDetailUserControl1.Designer.cs
+++ FinalProject24/ProductDetailUserControl1.Designer.cs
@@ -53,9 +53,10 @@
             // 
             // pictureBox1
             // 
-            pictureBox1.Location = new Point(71, 61);
+            pictureBox1.Location = new Point(81, 81);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(416, 277);
+            pictureBox1.Size = new Size(475, 369);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
@@ -63,9 +64,9 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(512, 45);
+            label1.Location = new Point(585, 60);
             label1.Name = "label1";
-            label1.Size = new Size(202, 37);
+            label1.Size = new Size(250, 46);
             label1.TabIndex = 12;
             label1.Text = "Product Name";
             // 
@@ -73,9 +74,9 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI Semibold", 17.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(512, 153);
+            label2.Location = new Point(585, 204);
             label2.Name = "label2";
-            label2.Size = new Size(129, 31);
+            label2.Size = new Size(162, 40);
             label2.TabIndex = 13;
             label2.Text = "Spice Level";
             // 
@@ -83,9 +84,9 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI Semibold", 17.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(512, 333);
+            label3.Location = new Point(585, 444);
             label3.Name = "label3";
-            label3.Size = new Size(197, 31);
+            label3.Size = new Size(249, 40);
             label3.TabIndex = 14;
             label3.Text = "Extra Condiments";
             // 
@@ -93,9 +94,10 @@
             // 
             checkBox1.AutoSize = true;
             checkBox1.Font = new Font("Segoe UI", 11.25F);
-            checkBox1.Location = new Point(517, 202);
+            checkBox1.Location = new Point(591, 269);
+            checkBox1.Margin = new Padding(3, 4, 3, 4);
             checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(83, 24);
+            checkBox1.Size = new Size(104, 29);
             checkBox1.TabIndex = 15;
             checkBox1.Text = "Medium";
             checkBox1.UseVisualStyleBackColor = true;
@@ -104,9 +106,10 @@
             // 
             checkBox2.AutoSize = true;
             checkBox2.Font = new Font("Segoe UI", 11.25F);
-            checkBox2.Location = new Point(517, 239);
+            checkBox2.Location = new Point(591, 319);
+            checkBox2.Margin = new Padding(3, 4, 3, 4);
             checkBox2.Name = "checkBox2";
-            checkBox2.Size = new Size(53, 24);
+            checkBox2.Size = new Size(64, 29);
             checkBox2.TabIndex = 16;
             checkBox2.Text = "Hot";
             checkBox2.UseVisualStyleBackColor = true;
@@ -115,9 +118,10 @@
             // 
             checkBox3.AutoSize = true;
             checkBox3.Font = new Font("Segoe UI", 11.25F);
-            checkBox3.Location = new Point(517, 281);
+            checkBox3.Location = new Point(591, 375);
+            checkBox3.Margin = new Padding(3, 4, 3, 4);
             checkBox3.Name = "checkBox3";
-            checkBox3.Size = new Size(111, 24);
+            checkBox3.Size = new Size(137, 29);
             checkBox3.TabIndex = 17;
             checkBox3.Text = "Extreme Hot";
             checkBox3.UseVisualStyleBackColor = true;
@@ -126,9 +130,10 @@
             // 
             checkBox4.AutoSize = true;
             checkBox4.Font = new Font("Segoe UI", 11.25F);
-            checkBox4.Location = new Point(517, 375);
+            checkBox4.Location = new Point(591, 500);
+            checkBox4.Margin = new Padding(3, 4, 3, 4);
             checkBox4.Name = "checkBox4";
-            checkBox4.Size = new Size(71, 24);
+            checkBox4.Size = new Size(88, 29);
             checkBox4.TabIndex = 18;
             checkBox4.Text = "Honey";
             checkBox4.UseVisualStyleBackColor = true;
@@ -137,9 +142,10 @@
             // 
             checkBox5.AutoSize = true;
             checkBox5.Font = new Font("Segoe UI", 11.25F);
-            checkBox5.Location = new Point(517, 414);
+            checkBox5.Location = new Point(591, 552);
+            checkBox5.Margin = new Padding(3, 4, 3, 4);
             checkBox5.Name = "checkBox5";
-            checkBox5.Size = new Size(129, 24);
+            checkBox5.Size = new Size(163, 29);
             checkBox5.TabIndex = 19;
             checkBox5.Text = "Honey Mustard";
             checkBox5.UseVisualStyleBackColor = true;
@@ -148,11 +154,12 @@
             // 
             checkBox6.AutoSize = true;
             checkBox6.Font = new Font("Segoe UI", 11.25F);
-            checkBox6.Location = new Point(517, 455);
+            checkBox6.Location = new Point(591, 607);
+            checkBox6.Margin = new Padding(3, 4, 3, 4);
             checkBox6.Name = "checkBox6";
-            checkBox6.Size = new Size(95, 24);
+            checkBox6.Size = new Size(119, 29);
             checkBox6.TabIndex = 20;
-            checkBox6.Text = "Hot Sause";
+            checkBox6.Text = "Hot Sauce";
             checkBox6.UseVisualStyleBackColor = true;
             // 
             // button1
@@ -161,9 +168,10 @@
             button1.FlatStyle = FlatStyle.Flat;
             button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             button1.ForeColor = SystemColors.ButtonHighlight;
-            button1.Location = new Point(512, 522);
+            button1.Location = new Point(585, 696);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(319, 41);
+            button1.Size = new Size(365, 55);
             button1.TabIndex = 21;
             button1.Text = "Add To Card";
             button1.UseVisualStyleBackColor = false;
@@ -172,9 +180,9 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label4.Location = new Point(519, 88);
+            label4.Location = new Point(593, 117);
             label4.Name = "label4";
-            label4.Size = new Size(56, 25);
+            label4.Size = new Size(71, 32);
             label4.TabIndex = 22;
             label4.Text = "$6.99";
             // 
@@ -182,9 +190,9 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label5.Location = new Point(66, 361);
+            label5.Location = new Point(75, 481);
             label5.Name = "label5";
-            label5.Size = new Size(108, 25);
+            label5.Size = new Size(135, 32);
             label5.TabIndex = 23;
             label5.Text = "Description";
             // 
@@ -192,26 +200,29 @@
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label6.Location = new Point(4, 10);
+            label6.Location = new Point(5, 13);
             label6.Name = "label6";
-            label6.Size = new Size(406, 80);
+            label6.Size = new Size(516, 100);
             label6.TabIndex = 24;
             label6.Text = resources.GetString("label6.Text");
             // 
             // groupBox1
             // 
             groupBox1.Controls.Add(label6);
-            groupBox1.Location = new Point(71, 389);
+            groupBox1.Location = new Point(81, 519);
+            groupBox1.Margin = new Padding(3, 4, 3, 4);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(416, 117);
+            groupBox1.Padding = new Padding(3, 4, 3, 4);
+            groupBox1.Size = new Size(475, 156);
             groupBox1.TabIndex = 25;
             groupBox1.TabStop = false;
             // 
             // button2
             // 
-            button2.Location = new Point(560, 493);
+            button2.Location = new Point(640, 657);
+            button2.Margin = new Padding(3, 4, 3, 4);
             button2.Name = "button2";
-            button2.Size = new Size(28, 23);
+            button2.Size = new Size(32, 31);
             button2.TabIndex = 26;
             button2.Text = "+";
             button2.UseVisualStyleBackColor = true;
@@ -220,9 +231,10 @@
             // button3
             // 
             button3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button3.Location = new Point(514, 493);
+            button3.Location = new Point(587, 657);
+            button3.Margin = new Padding(3, 4, 3, 4);
             button3.Name = "button3";
-            button3.Size = new Size(29, 23);
+            button3.Size = new Size(33, 31);
             button3.TabIndex = 27;
             button3.Text = "-";
             button3.UseVisualStyleBackColor = true;
@@ -232,15 +244,15 @@
             // 
             label7.AutoSize = true;
             label7.BackColor = SystemColors.ControlLight;
-            label7.Location = new Point(542, 497);
+            label7.Location = new Point(619, 663);
             label7.Name = "label7";
-            label7.Size = new Size(13, 15);
+            label7.Size = new Size(17, 20);
             label7.TabIndex = 28;
             label7.Text = "1";
             // 
             // ProductDetailUserControl1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             Controls.Add(label7);
             Controls.Add(button3);
@@ -259,8 +271,9 @@
             Controls.Add(label1);
             Controls.Add(pictureBox1);
             Controls.Add(groupBox1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "ProductDetailUserControl1";
-            Size = new Size(900, 600);
+            Size = new Size(1029, 800);
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
